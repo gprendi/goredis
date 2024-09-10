1af51bea7c3904384596dd80692bd7e9e0246759
Gerald Prendi <gprendi@outlook.com>
2024-09-10 09:02:36 +0200

    added resp

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
 )
 
 //TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
@@ -12,6 +17,60 @@ func main() {
 	// to see how GoLand suggests fixing it.
 	s := "gopher"
 	fmt.Println("Hello and welcome, %s!", s)
+	listen()
+	//readByte()
+}
+
+func readByte() {
+	input := "$5\r\nAhmed\r\n"
+	reader := bufio.NewReader(strings.NewReader(input))
+
+	b, _ := reader.ReadByte()
+
+	if b != '$' {
+		fmt.Println("Invalid type, expecting bulk strings only")
+		os.Exit(1)
+	}
+
+	size, _ := reader.ReadByte()
+
+	strSize, _ := strconv.ParseInt(string(size), 10, 64)
+
+	reader.ReadByte()
+	reader.ReadByte()
+
+	name := make([]byte, strSize)
+	reader.Read(name)
+	fmt.Println(string(name))
+}
+
+func listen() {
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		resp := NewResp(conn)
+		value, err := resp.Read()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(value)
+
+		// ignore request and send back a PONG
+		conn.Write([]byte("+OK\r\n"))
+	}
 
 }
 
